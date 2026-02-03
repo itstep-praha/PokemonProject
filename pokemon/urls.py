f925786df0a70b284e544cf5fd0a3b20fc518ac0
Suche Ganbaatar <113457666+suche-cz@users.noreply.github.com>
2026-02-03 20:52:02 +0100

    add views and templates

--- pokemon/urls.py
+++ pokemon/urls.py
@@ -1,57 +1,6 @@
-
-"""
-Dobré příklady:
-
-/pokemons/
-/pokemons/add/
-/pokemons/pikachu/
-/pokemons/pokachu/remove/
-/pokemons/pikachu/update/
-
-/cars/
-/cars/add/
-/cars/my-car/
-/cars/my-car/remove/
-/cars/my-car/update/
-
-
-list
-/
-
-detail
-/darek/10202/
-/darek/10202/vonna-svicka/
-/darek/vonna-svicka-10202/
-
-/darek/list/ - moje darky
-/darek/add/ - nový dárek
-/darek/10202/edit/ - editace (zobrazení i uložení)
-/darek/10202/delete/ - mazání
-
-
-youtube.com/
-youtube.com/feed/subscriptions/
-youtube.com/watch?v=87JKAIUSD87
-
---------------------------------------------------
-
-List Of Pokemons
-/
-
-Detail Of Pokemon
-/23/
-/pikachu/
-
-Cantact Page
-/contact/
-
-About Page
-/about/
-
-"""
-
 from django.urls import path
 from django.shortcuts import HttpResponse
+from pokemon import views
 
 
 def test_view(request, **kwargs):
@@ -59,15 +8,13 @@ def test_view(request, **kwargs):
     return HttpResponse('test page: ' + request.path)
 
 
-"""
-čím konkrétnější url cesta tím by měla být více nahoře
-čím více dynamická, více dole
-"""
-
 urlpatterns = [
-    path('', test_view),
-    path('about/', test_view),
-    path('contact/', test_view),
-    path('pokemon/<int:number>/', test_view),
-    path('pokemon/<slug:slug>/', test_view),
+    path('', views.index_view),
+    path('pokemon/', views.pokemon_list),
+    path('pokemon/<slug:pokemon_slug>/', views.pokemon_detail),
+
+
+    path('pokemon/<int:number>/', test_view), # pokemon_detail_number
+    path('about/', test_view), # about_view
+    path('contact/', test_view), # contact_view
 ]
--- pokemon/views.py
+++ pokemon/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from pokemon.models import Pokemon
 
-# Create your views here.
+
+def index_view(request):
+    return render(request, 'pokemon/index.html')
+
+
+def pokemon_list(request):
+    pokemons = Pokemon.objects.all()
+    return render(request, 'pokemon/pokemon_list.html', {'moje_jmeno': 'Nějaká hodnota', 'pokemons': pokemons})
+
+
+# @login_required
+def pokemon_detail(request, pokemon_slug):
+    return render(request, 'pokemon/pokemon_detail.html')
