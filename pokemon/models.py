0d7a136da58f56738b61afe5f685c849edc5e066
Suche Ganbaatar <113457666+suche-cz@users.noreply.github.com>
2026-02-24 23:06:00 +0100

    Change category colors

--- pokemon/models.py
+++ pokemon/models.py
@@ -34,31 +34,28 @@ class Category(models.Model):
         return self.name
     
     @property
-    def bootstrap_color(self):
-        try:
-            return category_to_bootstrap_color[self.slug]
-        except KeyError:
-            return 'secondary'
+    def color(self):
+        return CATEGORY_COLOR[self.slug]
 
 
-category_to_bootstrap_color = {
-    'grass': 'success',
-    'fire': 'danger',
-    'water': 'info',
-    'electric': 'warning',
-    'poison': 'secondary',
-    'ground': 'secondary',
-    'rock': 'secondary',
-    'bug': 'success',
-    'ghost': 'dark',
-    'steel': 'secondary',
-    'dragon': 'primary',
-    'psychic': 'danger',
-    'ice': 'info',
-    'fighting': 'danger',
-    'fairy': 'info',
-    'normal': 'secondary',
-    'flying': 'info',
+CATEGORY_COLOR = {
+    "bug": {"bg": "#A8B820", "text": "#FFFFFF"},
+    "dragon": {"bg": "#7038F8", "text": "#FFFFFF"},
+    "electric": {"bg": "#F8D030", "text": "#000000"},
+    "fairy": {"bg": "#EE99AC", "text": "#000000"},
+    "fighting": {"bg": "#C03028", "text": "#FFFFFF"},
+    "fire": {"bg": "#F08030", "text": "#FFFFFF"},
+    "flying": {"bg": "#A890F0", "text": "#000000"},
+    "ghost": {"bg": "#705898", "text": "#FFFFFF"},
+    "grass": {"bg": "#78C850", "text": "#000000"},
+    "ground": {"bg": "#E0C068", "text": "#000000"},
+    "ice": {"bg": "#98D8D8", "text": "#000000"},
+    "normal": {"bg": "#A8A878", "text": "#FFFFFF"},
+    "poison": {"bg": "#A040A0", "text": "#FFFFFF"},
+    "psychic": {"bg": "#F85888", "text": "#FFFFFF"},
+    "rock": {"bg": "#B8A038", "text": "#FFFFFF"},
+    "steel": {"bg": "#B8B8D0", "text": "#000000"},
+    "water": {"bg": "#6890F0", "text": "#FFFFFF"}
 }
 
 
